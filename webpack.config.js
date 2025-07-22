ed78092ac2a216cbb1dcc1961e7ae48a30b3939a
meineson <meineson@hotmail.com>
2025-07-22 14:47:48 +0800

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    mbphone: './mbphone.js'
+  },
+  // devtool: 'inline-source-map',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, './')
+  },optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin({
+      test: /\.js(\?.*)?$/i,
+      parallel: true,
+      terserOptions: {
+        compress: {
+          arrows: true, 
+          comparisons: false, 
+        },
+        mangle: true
+      },
+      extractComments: false, 
+    })],
+  }
+};
